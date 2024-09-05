50ad06b3c09f908b79a2389d7d373a87e7b0eb66
saviodc <savio.concessio@outlook.com>
2024-09-06 00:22:07 +1200

    Fully implemented both Client and Server and is running

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 public class Client {
 	Socket socket;
 	int port;
@@ -24,7 +25,7 @@ public class Client {
             if (in != null) in.close();
             if (out != null) out.close();
             if (socket != null && !socket.isClosed()) socket.close();
-            System.out.println("Socket closed successfully.");
+            //System.out.println("Socket closed successfully.");
         } catch (IOException e) {
             System.err.println("Error closing socket: " + e.getMessage());
             throw new UnsupportedOperationException("Failed to close the socket", e);
@@ -45,4 +46,26 @@ public class Client {
         return null;
     }
 	
+    
+    public static void main(String[] args) {
+    	String source = "3.86.247.185";//"embassy.ecs.vuw.ac.nz";
+    	int port = 3000;
+    	Client c = new Client(source,port);
+    	//System.out.println("Input question: ");
+    	Scanner in = new Scanner(System.in);
+    	String request = "Is today a good day?";//in.nextLine();
+    
+    	c.sendMessage(request);
+    	System.out.println("Magic8BallClient "+ source + " "+ port + " \""+ request+"\"");
+    	try {
+			String input = c.receiveMessage();
+			System.out.println("Magic 8  Ball says: "+ input);
+			
+		} catch (IOException e) {
+			System.out.println("Error Reading: \n"+e.getMessage());	
+		}
+    	c.closeSocket();
+    	in.close();
+    }
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+	static String[] answers = {"Yes definitely", "No definitely", "Not sure, come again", "Why not"};
+	ServerSocket server;
+	Socket client;
+	int port;
+	BufferedReader in;
+	PrintWriter out;
+	public Server(int port) {
+		this.port = port;
+    	initSocket();
+	}
+	 public void sendMessage(String message) {
+	        if (out != null) {
+	            out.println(message);
+	        }
+	    }
 
+	    // Method to read a response from the server
+	    public String receiveMessage() throws IOException {
+	        if (in != null) {
+	            return in.readLine();
+	        }
+	        return null;
+	    }
+	private void initSocket() {
+		try {
+			server = new ServerSocket(port);
+			System.out.println("Established @ "+ port);
+			
+				client = server.accept();
+				System.out.println("Client found");
+				out = new PrintWriter(client.getOutputStream(), true);
+		        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		        receiveMessage();
+		        sendMessage(answers[((int)Math.random()*answers.length)]);
+		        
+		        
+			
+		} catch (IOException e) {
+			
+		}
+		
+	}
+	
+	public void find() {
+		try {
+			client = server.accept();
+		
+		System.out.println("Client found");
+		out = new PrintWriter(client.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        System.out.println(receiveMessage());
+        sendMessage(answers[((int)Math.random()*answers.length)]);
+        client.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Server s = new Server(3000);
+		while(true) {
+			s.find();
+		}
+	}
 }
